d0e909937c48dc3e915f90244492586026be5942
dhoomakethu <otlasanju@gmail.com>
2020-02-08 14:05:07 +0530

    Travis OSX latest xcode experiments

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ "$TRAVIS_OS_NAME" = osx ]; then
-  VIRTUAL_ENV="$HOME/.virtualenvs/python2.7"
+  VIRTUAL_ENV="$HOME/.virtualenvs/python3.7"
   if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
     virtualenv "$VIRTUAL_ENV"
   fi
